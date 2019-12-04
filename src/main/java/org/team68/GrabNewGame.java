14489d43d7ef2a50abb8443becac5da712280d64
qgeff <q.geffard@gmail.com>
2019-12-04 18:34:34 -0500

    Create the maven project

---
+++ src/main/java/org/team68/GrabNewGame.java
@@ -0,0 +1,12 @@
+package org.team68;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrabNewGame {
+    private Logger log = LoggerFactory.getLogger(GrabNewGame.class);
+
+    public void execute(){
+        log.info("Hello Mede & Quentin, I will grab your free weekly game for you");
+    }
+}
---
+++ src/main/java/org/team68/Launcher.java
@@ -0,0 +1,9 @@
+package org.team68;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        GrabNewGame grabNewGame = new GrabNewGame();
+        grabNewGame.execute();
+    }
+}
